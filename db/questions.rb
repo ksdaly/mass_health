fc7edd6b83a91fecb9287a503cc3e347b30df407
Kate Daly <kate.s.daly@gmail.com>
2013-09-05 15:46:21 -0400

    cleanup

--- db/questions.rb
+++ db/questions.rb
@@ -1,7 +1,7 @@
 # What 3 towns have the highest population of citizens that are 65 years and older?
-TownHealthRecord.order("age_65_plus DESC").limit(3)
+TownHealthRecord.order('age_65_plus DESC').limit(3)
 # What 3 towns have the highest population of citizens that are 19 years and younger?
-TownHealthRecord.order("age_0_19 DESC").limit(3)
+TownHealthRecord.order('age_0_19 DESC').limit(3)
 # What 5 towns have the lowest per capita income?
 TownHealthRecord.order(:per_capita_income).limit(5)
 # Omitting Boston, Becket, and Beverly, what town has the highest percentage of teen births?
